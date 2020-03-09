77ac6ebcf39896f70e2f464a4746ed530791bced
root <ali.darwich@etu.unistra.fr>
2020-03-09 21:56:36 +0100

    modif le nav bar

--- templates/navbar.php
+++ templates/navbar.php
@@ -5,22 +5,17 @@
     <title>navbar</title>
     <link rel="stylesheet" type="text/css" href="/adarwich/programmation-web-2---s4---2020/assets/navbar.css">
   </head>
-  <body>
-    <div class="navbar">
-      <ul>
-        <li> <a href="/adarwich/programmation-web-2---s4---2020/index.php">Home</a></li>
-        <li> <a href="/adarwich/programmation-web-2---s4---2020/assets/communication.php">communication</a></li>
-        <li> <a href="/adarwich/programmation-web-2---s4---2020/assets/contactus.php">contact us</a></li>
-        <li> <a href="/adarwich/programmation-web-2---s4---2020/assets/aboutus.php">about us</a></li>
-      </ul>
-    </div>
-    <div class="compte_bar">
-      <ul>
-        <li> <a href="/adarwich/programmation-web-2---s4---2020/assets/myaccount.php">My Account</a></li>
-        <li> <a href="/adarwich/programmation-web-2---s4---2020/assets/signin.php">Sign In</a></li>
-        <li> <a href="/adarwich/programmation-web-2---s4---2020/assets/signup.php">Sign up</a></li>
-      </ul>
-    </div>
-
-  </body>
+    <body>
+        <div class="navbar">
+            <ul>
+                <li> <a href="/adarwich/programmation-web-2---s4---2020/index.php">Home</a></li>
+                <li> <a href="/adarwich/programmation-web-2---s4---2020/assets/communication.php">communication</a></li>
+                <li> <a href="/adarwich/programmation-web-2---s4---2020/assets/contactus.php">contact us</a></li>
+                <li> <a href="/adarwich/programmation-web-2---s4---2020/assets/aboutus.php">about us</a></li>
+                <li class="compte"> <a href="/adarwich/programmation-web-2---s4---2020/assets/myaccount.php">My Account</a><li>
+                <li class="compte"> <a href="/adarwich/programmation-web-2---s4---2020/assets/signin.php">Sign In</a></li>
+                <li class="compte"> <a href="/adarwich/programmation-web-2---s4---2020/assets/signup.php">Sign up</a></li>
+            </ul>
+        </div>
+    </body>
 </html>
