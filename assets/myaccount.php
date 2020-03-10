03483a1fb174785e3e0fa154c86413385f8700ce
DARWICH ALI <ali.darwich@etu.unistra.fr>
2020-03-10 09:59:58 +0100

    mod myacc

--- assets/myaccount.php
+++ assets/myaccount.php
@@ -12,13 +12,22 @@
 	<?php include '../templates/aside.php'; ?>
 	<div class="">
 		<img src="#">
-		<p>prenom :</p>
-		<p>nom :</p>
-		<p>E-mail :</p>
+		<p>prenom :</p><p id="prenom"></p>
+		<p>nom :</p><p id="nom"></p>
+		<p>E-mail :</p><p id="email"></p>
+		<select id="mylist">
+			<option></option>
+			<option>prenom</option>
+			<option>nom</option>
+		</select>
+		<button id="button_sign" type="button">Créer mon compte</button>
+		<p>change to: <input type="text" id="Val" size="20"></p>
+
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		
 	</div>
 
 	<?php include '../templates/footer.php'; ?>
 </body>
+<script src="myaccount.js"></script>
 </html>
