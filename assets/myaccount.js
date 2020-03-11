c81c798664bec1d43bdc28a6321049e9945d118a
root <ali.darwich@etu.unistra.fr>
2020-03-11 18:59:50 +0100

    modif en myaccount

--- assets/myaccount.js
+++ assets/myaccount.js
@@ -1,16 +1,23 @@
 
 
 
-mylist.onchange = function() {
-	alert("lol");
+button_sign.onclick = function() {
+	//alert("lol");
 	var txt = document.getElementById("Val").value
+	//alert(txt);
+	//alert(mylist.options[mylist.selectedIndex].text);
 	if(mylist.options[mylist.selectedIndex].text == "prenom"){
+		//alert("i'm in");
+		let tmp = document.getElementById("prenom").innerHTML ;
+		document.getElementById("prenom").innerHTML = tmp +" " +txt ;
 
 	}
-	if (mylist.options[mylist.selectedIndex].text == "prenom"){
+	if (mylist.options[mylist.selectedIndex].text == "nom"){
+		 tmp = document.getElementById("nom").innerHTML ;
+		document.getElementById("nom").innerHTML = tmp + " " +txt ;
 
 	}
-	
-	document.getElementById(#id).value 
+
+
 
 }
